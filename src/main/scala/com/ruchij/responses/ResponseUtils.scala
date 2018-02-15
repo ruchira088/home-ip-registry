34ba214d9cd4a79c3fab797e05afc5e2d16223da
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-02-15 13:24:50 +1100

    Refactoring

--- src/main/scala/com/ruchij/responses/ResponseUtils.scala
+++ src/main/scala/com/ruchij/responses/ResponseUtils.scala
@@ -1,31 +1,36 @@
 package com.ruchij.responses
 
 import akka.http.scaladsl.marshalling.{Marshaller, ToResponseMarshallable, ToResponseMarshaller}
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{Route, StandardRoute}
+import akka.http.scaladsl.server.{ExceptionHandler, Route, StandardRoute}
+import com.ruchij.exceptions.{InvalidAuthTokenException, MissingAuthTokenException}
 
 import scala.util.Try
 import scala.util.control.NonFatal
 
 object ResponseUtils
 {
-  implicit def exceptionMarshaller: Marshaller[Throwable, HttpResponse] =
-    Marshaller.opaque {
-      case NonFatal(throwable) =>
-        HttpResponse(
-          status = StatusCodes.InternalServerError,
-          entity = HttpEntity(ContentTypes.`application/json`, s"""{"error": "${throwable.getMessage}"}""")
-        )
+  private def errorResponse(statusCode: StatusCode, throwable: Throwable): StandardRoute =
+    complete {
+      HttpResponse(
+        status = statusCode,
+        entity = HttpEntity(ContentTypes.`application/json`, s"""{"error": "${throwable.getMessage}"}""")
+      )
     }
 
-  def tryResultHandler[A](result: Try[A])(implicit toResponseMarshaller: ToResponseMarshaller[A]): Route =
+  def exceptionHandler = ExceptionHandler {
+    case MissingAuthTokenException => errorResponse(StatusCodes.Unauthorized, MissingAuthTokenException)
+    case InvalidAuthTokenException => errorResponse(StatusCodes.Unauthorized, InvalidAuthTokenException)
+  }
+
+  def tryResultHandler[A](errorHandler: PartialFunction[Throwable, Route] = PartialFunction.empty)(result: Try[A])(implicit toResponseMarshaller: ToResponseMarshaller[A]): Route =
     result.fold[Route](
-      {
+      errorHandler.orElse {
         case NonFatal(throwable) =>
-          complete(ToResponseMarshallable(throwable))
-      },
+          errorResponse(StatusCodes.InternalServerError, throwable)
+      }
+     ,
       value => complete(ToResponseMarshallable(value))
     )
-
 }
--- src/main/scala/com/ruchij/routes/IndexRoute.scala
+++ src/main/scala/com/ruchij/routes/IndexRoute.scala
@@ -1,33 +1,43 @@
 package com.ruchij.routes
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.directives.AuthenticationDirective
+import com.ruchij.exceptions.EmptyDatabaseException
 import com.ruchij.responses.ResponseUtils._
 import com.ruchij.services.PingService
 
 object IndexRoute
 {
   def apply(pingService: PingService, authentication: AuthenticationDirective[_]) =
-    path("") {
-      get {
-        complete("Hello World")
-      }
-    } ~
-    path("heart-beat") {
-      post {
-        authentication { _ =>
-          extractClientIP {
-            clientIp =>
-              onComplete(pingService.insert(clientIp))(tryResultHandler)
+    handleExceptions(exceptionHandler) {
+      path("") {
+        get {
+          complete("Hello World")
+        }
+      } ~
+        path("heart-beat") {
+          post {
+            authentication { _ =>
+              extractClientIP {
+                clientIp =>
+                  onComplete(pingService.insert(clientIp)) {
+                    tryResultHandler()
+                  }
+              }
+            }
+          }
+        } ~
+        path("home-ip") {
+          get {
+            authentication { _ =>
+              onComplete(pingService.getLatestIp()) {
+                tryResultHandler {
+                  case EmptyDatabaseException => complete(StatusCodes.NoContent)
+                }
+              }
+            }
           }
         }
-      }
-    } ~
-    path("home-ip") {
-      get {
-        authentication { _ =>
-          onComplete(pingService.getLatestIp())(tryResultHandler)
-        }
-      }
     }
 }
